02d772b4da8802b5dbc0ee809a82e85faab92ada
Lasha Markhvaidze <lasha.marxvaidze@gmail.com>
2021-03-21 14:17:43 +0400

    play/pause button

--- myscripts.js
+++ myscripts.js
@@ -1,1 +1,13 @@
-
+var sound = document.getElementById("audio");
+function audio(){
+    sound.currentTime = 0;
+    sound.volume = 0.15;
+    sound.loop = true; //if you want it to restart playing automatically when it ends
+}
+audio();
+function play(){
+    sound.play();
+}
+function pause(){
+    sound.pause();
+}
