4ac1785b1902ef7b868ca1d4aca59f9429696a1e
Lasha Markhvaidze <lasha.marxvaidze@gmail.com>
2021-03-23 17:35:04 +0400

    Moving + Tesla + Gravity + Rotation + Reset

--- myscripts.js
+++ myscripts.js
@@ -1,13 +1,20 @@
 var sound = document.getElementById("audio");
-var ball = document.getElementById('ball');
-let l = 0;
-var b = -100;
+var ball = document.getElementById('tesla');
+var left = 0;
+var topp = 0;
+var height = window.innerHeight;
+var width = window.innerWidth;
+var deg = 0;
+
 
 audio();
 move();
+window.onscroll = function () {
+    window.scrollTo(0,0);
+    }
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
+}
 function audio(){
     sound.currentTime = 0;
     sound.volume = 0.15;
@@ -22,20 +29,25 @@ function pause(){
 
 
 function move(){
-    
+    left= -20;
+    topp= -20;
     setInterval(function(){
-        l+=1;  
-        b+=1;
-        ball.style.left = l + "px";
-        ball.style.top = b + "px";
-    
-        if(l>500){  //amaze samushao
-            var r = random(-100, 1500);
-            var j = random(-200, -300);
-            l=r;
-            b=j;
+        left+=0.02;  
+        topp+=0.02;
+        deg +=0.1;
+        ball.style.left = left + "%";
+        ball.style.top = topp + "%";
+        ball.style.transform = "rotate(" + deg +"deg)"
+        if(topp>100 || left>100){  //amaze samushao
+            var r = random(-30, 100);
+            var j = -80
+            left=r;
+            topp=j;
         }
-    }, 15); 
+
+        
+    }, 1); 
 
 }
+
 
