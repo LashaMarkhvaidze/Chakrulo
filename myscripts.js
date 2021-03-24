7027c4c531a46d1a02a21267f9354c7149c5e531
Lasha Markhvaidze <lasha.marxvaidze@gmail.com>
2021-03-24 16:35:32 +0400

    Added BOG
    მოიცავს: ჩაკრულოს, ტესლას და "გზა სულ არის" - ს

--- myscripts.js
+++ myscripts.js
@@ -1,14 +1,22 @@
 var sound = document.getElementById("audio");
 var ball = document.getElementById('tesla');
+var gza = document.getElementById('gza');
+
 var left = 0;
 var topp = 0;
 var height = window.innerHeight;
 var width = window.innerWidth;
 var deg = 0;
 
+var left1 = 0;
+var topp1 = 0;
+var deg1 = 0;
+
 
 audio();
 move();
+gzaf();
+
 window.onscroll = function () {
     window.scrollTo(0,0);
     }
@@ -39,7 +47,7 @@ function move(){
         ball.style.top = topp + "%";
         ball.style.transform = "rotate(" + deg +"deg)"
         if(topp>100 || left>100){  //amaze samushao
-            var r = random(-30, 100);
+            var r = random(-80, 80);
             var j = -80
             left=r;
             topp=j;
@@ -47,6 +55,29 @@ function move(){
 
         
     }, 1); 
+
+}
+
+function gzaf(){
+    left1 = 150;
+    topp1 = -100;
+    
+    setInterval(function(){
+        left1-=0.02;  
+        topp1+=0.02;
+        deg1 -=0.1;
+        gza.style.left = left1 + "%";
+        gza.style.top = topp1 + "%";
+        gza.style.transform = "rotate(" + deg1 +"deg)"
+        
+
+        if(topp1>180|| left1<-50){  //amaze samushao
+            var t = random(100, 200);
+            var y = -80
+            left1=t;
+            topp1=y;
+        }        
+    }, 1);
 
 }
 
